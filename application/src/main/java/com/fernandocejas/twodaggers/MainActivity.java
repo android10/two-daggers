289df9aec59252c31241e880d87398acc082f858
Dawid <dawidhyzy@gmail.com.com>
2017-11-29 09:37:21 +0400

    Updated dependencies to work with Android Studio 3.0

--- application/src/main/java/com/fernandocejas/twodaggers/MainActivity.java
+++ application/src/main/java/com/fernandocejas/twodaggers/MainActivity.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.fernandocejas.twodaggers.di.Dagger1Module;
@@ -13,12 +13,12 @@ import com.fernandocejas.twodaggers.di.Dagger2Module;
 import com.fernandocejas.twodaggers.di.DaggerDagger2Component;
 import dagger.ObjectGraph;
 import javax.inject.Inject;
-import javax.inject.Inject2;
+//import javax.inject.Inject2;
 
 public class MainActivity extends Activity {
 
-  @Bind(R.id.txt_dagger1) TextView txt_dagger1;
-  @Bind(R.id.txt_dagger2) TextView txt_dagger2;
+  @BindView(R.id.txt_dagger1) TextView txt_dagger1;
+  @BindView(R.id.txt_dagger2) TextView txt_dagger2;
 
   @Inject Dagger1DataManager dagger1DataManager;
   @Inject2 Dagger2DataManager dagger2DataManager;
--- application/src/main/java/com/fernandocejas/twodaggers/StringGenerator.java
+++ application/src/main/java/com/fernandocejas/twodaggers/StringGenerator.java
@@ -1,16 +1,16 @@
 package com.fernandocejas.twodaggers;
 
 import javax.inject.Inject;
-import javax.inject.Inject2;
+//import javax.inject.Inject2;
 import javax.inject.Singleton;
-import javax.inject.Singleton2;
+//import javax.inject.Singleton2;
 
 @Singleton
-@Singleton2
+//@Singleton2
 public class StringGenerator {
 
   @Inject
-  @Inject2
+//  @Inject2
   /**
    * This class can be injected by both dagger 1 and 2
    */
